1a7a85d7bc493a8757e3aa42cee964107bd57c60
Peter Sagdahl <peter.sagdahl@gmail.com>
2022-03-15 23:37:55 +0100

    #GUI lagt til login

--- Database/query.py
+++ Database/query.py
@@ -5,6 +5,7 @@ from sqlalchemy import null
 
 
 
+
 class queries:
     
     def __init__(self, databaseName):
@@ -13,9 +14,24 @@ class queries:
         
 
     @classmethod
-    def register(self):
+    def register(self,brenneri, kaffenavn, poeng, smaksnotat, email):
         self.cursor.execute("""
-        Insert into
-        """)
+        Insert into kaffebrenneri VALUES (:brenneri);
+        Insert into kaffesmaking (SID, Notater, Poeng, Smaksdato, Email, Kaffenavn, Brennerinavn) 
+        Values (:Notater, :Poeng, :Email, :Kaffenavn)
+        """, {"brenneri" : brenneri, "Kaffenavn" : kaffenavn, "Notater" : smaksnotat, "Poeng" : poeng, "Email" : email})
+        self.con.commit()
         self.con.close
+
+    @classmethod
+    def getUser(self, email, password):
+        self.cursor.execute("""
+        """, {})
+
+        forname = null
+        surname = null
+
+        self.con.commit()
+        self.con.close
+        return forname, surname
 
